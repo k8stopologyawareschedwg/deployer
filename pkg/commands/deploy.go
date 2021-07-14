19a0a9d3a8346c91798189b8cdfdbe37a029c9ad
Talor Itzhak <titzhak@redhat.com>
2021-07-14 13:50:34 +0300

    Deploy/Undeploy CRD
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- pkg/commands/deploy.go
+++ pkg/commands/deploy.go
@@ -17,18 +17,23 @@
 package commands
 
 import (
+	"context"
+	"github.com/fromanirh/deployer/pkg/clientutil"
+	"github.com/fromanirh/deployer/pkg/manifests"
 	"github.com/spf13/cobra"
 )
 
 type deployOptions struct{}
 
 func NewDeployCommand(commonOpts *CommonOptions) *cobra.Command {
-	//	opts := &deployOptions{}
+	opts := &deployOptions{}
 	deploy := &cobra.Command{
 		Use:   "deploy",
 		Short: "deploy the components and configurations needed for topology-aware-scheduling",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: deploy components here
+			if err := Deploy(opts); err != nil {
+				return err
+			}
 			return nil
 		},
 		Args: cobra.NoArgs,
@@ -37,15 +42,53 @@ func NewDeployCommand(commonOpts *CommonOptions) *cobra.Command {
 }
 
 func NewRemoveCommand(commonOpts *CommonOptions) *cobra.Command {
-	//	opts := &deployOptions{}
+	opts := &deployOptions{}
 	remove := &cobra.Command{
 		Use:   "remove",
 		Short: "remove the components and configurations needed for topology-aware-scheduling",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: remove components here
+			if err := Remove(opts); err != nil {
+				return err
+			}
 			return nil
 		},
 		Args: cobra.NoArgs,
 	}
 	return remove
+}
+
+func Deploy(opts *deployOptions) error {
+	cs, err := clientutil.New()
+	if err != nil {
+		return err
+	}
+
+	crd, err := manifests.APICRD()
+	if err != nil {
+		return err
+	}
+
+	err = cs.Create(context.TODO(), crd)
+	if err !=nil {
+		return err
+	}
+	return nil
+}
+
+func Remove(opts *deployOptions) error {
+	cs, err := clientutil.New()
+	if err != nil {
+		return err
+	}
+
+	crd, err := manifests.APICRD()
+	if err != nil {
+		return err
+	}
+
+	err = cs.Delete(context.TODO(), crd)
+	if err !=nil {
+		return err
+	}
+	return nil
 }
