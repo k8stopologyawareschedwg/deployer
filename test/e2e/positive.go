60e87f3556d08d9450e6a26c075ad4968aa71601
Francesco Romani <fromani@redhat.com>
2021-07-20 09:29:27 +0200

    e2e: deploy: generalize the deploy test
    minor reliability improvements to the deployment test:
    1. don't assume we will only always have 1 worker node
    2. general gofmt pass
    3. minor misc fixes
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/positive.go
+++ test/e2e/positive.go
@@ -26,18 +26,22 @@ import (
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/k8stopologyawareschedwg/noderesourcetopology-api/pkg/apis/topology/v1alpha1"
+	topologyclientset "github.com/k8stopologyawareschedwg/noderesourcetopology-api/pkg/generated/clientset/versioned"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 
 	"github.com/fromanirh/deployer/pkg/clientutil"
+	"github.com/fromanirh/deployer/pkg/clientutil/nodes"
 	"github.com/fromanirh/deployer/pkg/manifests/rte"
 	"github.com/fromanirh/deployer/pkg/manifests/sched"
 )
+
 const (
-	// the name of the nrt object is the same as the worker node's name
-	nodeResourceTopologyName = "kind-worker"
+	// TODO: this should be autodetected
 	exportedNs = "default"
 )
 
@@ -65,7 +69,9 @@ var _ = ginkgo.Describe("[PositiveFlow] Deployer validation", func() {
 			gomega.Expect(vo.Errors).To(gomega.BeEmpty())
 		})
 	})
+})
 
+var _ = ginkgo.Describe("[PositiveFlow] Deployer execution", func() {
 	ginkgo.Context("with a running cluster without any components", func() {
 		ginkgo.BeforeEach(func() {
 			err := deploy()
@@ -82,69 +88,75 @@ var _ = ginkgo.Describe("[PositiveFlow] Deployer validation", func() {
 			mf, err := rte.GetManifests()
 			gomega.Expect(err).ToNot(gomega.HaveOccurred())
 			mf = mf.UpdateNamespace()
-
-			gomega.Eventually(func() error {
-				pod, err := getPodByRegex(fmt.Sprintf("%s-*", mf.DaemonSet.Name))
-				if err != nil {
-					return err
-				}
-
-				if pod.Status.Phase == corev1.PodRunning {
-					return nil
-				}
-
-				return fmt.Errorf("pod %v is not in %v state", pod.Name, corev1.PodRunning)
-			}, 1 * time.Minute, 15 * time.Second).ShouldNot(gomega.HaveOccurred())
+			waitPodsToBeRunningByRegex(fmt.Sprintf("%s-*", mf.DaemonSet.Name))
 
 			ginkgo.By("checking that topo-aware-scheduler pod is running")
 			mfs, err := sched.GetManifests()
 			gomega.Expect(err).ToNot(gomega.HaveOccurred())
 			mfs = mfs.UpdateNamespace()
-
-			gomega.Eventually(func() error {
-				pod, err := getPodByRegex(fmt.Sprintf("%s-*", mfs.Deployment.Name))
-				if err != nil {
-					return err
-				}
-
-				if pod.Status.Phase == corev1.PodRunning {
-					return nil
-				}
-
-				return fmt.Errorf("pod %v is not in %v state", pod.Name, corev1.PodRunning)
-			}, 1 * time.Minute, 15 * time.Second).ShouldNot(gomega.HaveOccurred())
+			waitPodsToBeRunningByRegex(fmt.Sprintf("%s-*", mfs.Deployment.Name))
 
 			ginkgo.By("checking that noderesourcetopolgy has some information in it")
 			tc, err := clientutil.NewTopologyClient()
 			gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
-			gomega.Eventually(func() error {
-				_, err := tc.TopologyV1alpha1().NodeResourceTopologies(exportedNs).Get(context.TODO(), nodeResourceTopologyName, metav1.GetOptions{})
-				if err != nil {
-					return err
-				}
-				return nil
-			}, 1 * time.Minute, 15 * time.Second).ShouldNot(gomega.HaveOccurred())
+			workers, err := nodes.GetWorkers()
 			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			for _, node := range workers {
+				ginkgo.By(fmt.Sprintf("checking node resource topology for %q", node.Name))
 
-			nrt, err := tc.TopologyV1alpha1().NodeResourceTopologies(exportedNs).Get(context.TODO(), nodeResourceTopologyName, metav1.GetOptions{})
-			gomega.Expect(err).ToNot(gomega.HaveOccurred())
-
-			hasCPU := false
-			for _, zone := range nrt.Zones {
-				for _, resource := range zone.Resources {
-					if resource.Name == string(corev1.ResourceCPU) && resource.Capacity.Size() >= 1{
-						hasCPU = true
+				// the name of the nrt object is the same as the worker node's name
+				nrt := getNodeResourceTopology(tc, exportedNs, node.Name)
+				// we check CPUs because that's the only resource we know it will always be available
+				hasCPU := false
+				for _, zone := range nrt.Zones {
+					for _, resource := range zone.Resources {
+						if resource.Name == string(corev1.ResourceCPU) && resource.Capacity.Size() >= 1 {
+							hasCPU = true
+						}
 					}
 				}
+				gomega.Expect(hasCPU).To(gomega.BeTrue())
+				gomega.Expect(nrt.TopologyPolicies[0]).ToNot(gomega.BeEmpty())
 			}
-			gomega.Expect(hasCPU).To(gomega.BeTrue())
-			gomega.Expect(nrt.TopologyPolicies[0]).ToNot(gomega.BeNil())
 		})
 	})
 })
 
-func getPodByRegex(reg string) (*corev1.Pod, error){
+func getNodeResourceTopology(tc *topologyclientset.Clientset, namespace, name string) *v1alpha1.NodeResourceTopology {
+	var err error
+	var nrt *v1alpha1.NodeResourceTopology
+	gomega.EventuallyWithOffset(1, func() error {
+		nrt, err = tc.TopologyV1alpha1().NodeResourceTopologies(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		return nil
+	}, 1*time.Minute, 15*time.Second).ShouldNot(gomega.HaveOccurred())
+	gomega.ExpectWithOffset(1, err).ToNot(gomega.HaveOccurred())
+	return nrt
+}
+
+func waitPodsToBeRunningByRegex(pattern string) {
+	gomega.EventuallyWithOffset(1, func() error {
+		pods, err := getPodsByRegex(fmt.Sprintf("%s-*", pattern))
+		if err != nil {
+			return err
+		}
+		if len(pods) == 0 {
+			return fmt.Errorf("no pods found for %q", pattern)
+		}
+
+		for _, pod := range pods {
+			if pod.Status.Phase != corev1.PodRunning {
+				return fmt.Errorf("pod %q is not in %v state", pod.Name, corev1.PodRunning)
+			}
+		}
+		return nil
+	}, 1*time.Minute, 15*time.Second).ShouldNot(gomega.HaveOccurred())
+}
+
+func getPodsByRegex(reg string) ([]*corev1.Pod, error) {
 	cs, err := clientutil.New()
 	if err != nil {
 		return nil, err
@@ -161,44 +173,45 @@ func getPodByRegex(reg string) (*corev1.Pod, error){
 		return nil, err
 	}
 
+	ret := []*v1.Pod{}
 	for _, pod := range podList.Items {
 		if match := podNameRgx.FindString(pod.Name); len(match) != 0 {
-			return &pod, nil
+			ret = append(ret, &pod)
 		}
 	}
-	return nil, fmt.Errorf("pod starting with %v does not exist", reg)
+	return ret, nil
 }
 
 func deploy() error {
-		cmdline := []string{
-			filepath.Join(binariesPath, "deployer"),
-			"deploy",
-		}
-		fmt.Fprintf(ginkgo.GinkgoWriter, "running: %v\n", cmdline)
+	cmdline := []string{
+		filepath.Join(binariesPath, "deployer"),
+		"deploy",
+	}
+	fmt.Fprintf(ginkgo.GinkgoWriter, "running: %v\n", cmdline)
 
-		cmd := exec.Command(cmdline[0], cmdline[1:]...)
-		cmd.Stdout = ginkgo.GinkgoWriter
-		cmd.Stderr = ginkgo.GinkgoWriter
+	cmd := exec.Command(cmdline[0], cmdline[1:]...)
+	cmd.Stdout = ginkgo.GinkgoWriter
+	cmd.Stderr = ginkgo.GinkgoWriter
 
-		if err := cmd.Run(); err != nil {
-			return fmt.Errorf("failed to deploy componentes before test started: %v", err)
-		}
-		return nil
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to deploy componentes before test started: %v", err)
+	}
+	return nil
 }
 
 func remove() error {
-		cmdline := []string{
-			filepath.Join(binariesPath, "deployer"),
-			"remove",
-		}
-		fmt.Fprintf(ginkgo.GinkgoWriter, "running: %v\n", cmdline)
+	cmdline := []string{
+		filepath.Join(binariesPath, "deployer"),
+		"remove",
+	}
+	fmt.Fprintf(ginkgo.GinkgoWriter, "running: %v\n", cmdline)
 
-		cmd := exec.Command(cmdline[0], cmdline[1:]...)
-		cmd.Stdout = ginkgo.GinkgoWriter
-		cmd.Stderr = ginkgo.GinkgoWriter
+	cmd := exec.Command(cmdline[0], cmdline[1:]...)
+	cmd.Stdout = ginkgo.GinkgoWriter
+	cmd.Stderr = ginkgo.GinkgoWriter
 
-		if err := cmd.Run(); err != nil {
-			return fmt.Errorf("failed to remove components after test finished: %v", err)
-		}
-		return nil
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to remove components after test finished: %v", err)
+	}
+	return nil
 }
