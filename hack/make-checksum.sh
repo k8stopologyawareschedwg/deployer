8b79c098f15f1630e88dce9f7c661bd101a0157f
Francesco Romani <fromani@redhat.com>
2022-02-16 07:19:56 +0100

    gh actions: release: simplify checksumming
    Add a helper to simplify the checksum creation in the
    release flow. Also makes the flow a bit easier to test.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ hack/make-checksum.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+VERSION="${1}"
+FILES="
+deployer-${VERSION}-linux-amd64
+deployer-manifests-allinone-${VERSION}.yaml
+"
+
+for artifact in $FILES; do
+	if [ ! -f "_out/${artifact}" ]; then
+		echo "MISSING: ${artifact}" >&2
+		exit 1
+	fi
+done
+
+pushd _out
+:> SHA256SUMS
+sha256sum ${FILES} >> SHA256SUMS
+popd
