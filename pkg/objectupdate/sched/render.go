e44a6c7683eb966d2ac3c7e3cbe3ef687d149f2d
Francesco Romani <fromani@redhat.com>
2024-02-22 17:40:41 +0100

    objectupdate: sched: be louder on unexpected conditions
    If something should never happen, let's be loud about it
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/objectupdate/sched/render.go
+++ pkg/objectupdate/sched/render.go
@@ -70,7 +70,7 @@ func RenderConfig(data []byte, schedulerName string, params *manifests.ConfigPar
 	for _, prof := range profiles {
 		profile, ok := prof.(map[string]interface{})
 		if !ok {
-			klog.V(1).InfoS("unexpected profile data")
+			klog.InfoS("unexpected profile data")
 			return data, false, nil
 		}
 
