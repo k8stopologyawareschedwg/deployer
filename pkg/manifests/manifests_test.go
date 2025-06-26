74bc1770733140ae7882d53a39eefbe350d340af
Ronny Baturov <rbaturov@redhat.com>
2025-06-26 13:53:18 +0300

    add unit tests for network policies
    Signed-off-by: Ronny Baturov <rbaturov@redhat.com>

--- pkg/manifests/manifests_test.go
+++ pkg/manifests/manifests_test.go
@@ -562,3 +562,102 @@ func TestSecurityContextConstraint(t *testing.T) {
 		})
 	}
 }
+
+func TestNetworkPolicy(t *testing.T) {
+	testCases := []struct {
+		description  string
+		policyType   string
+		policyName   string
+		component    string
+		subComponent string
+		policyExist  bool
+	}{
+		{
+			description:  "rte default network policy",
+			policyType:   "default",
+			policyName:   "rte-default-deny-all",
+			component:    ComponentResourceTopologyExporter,
+			subComponent: "",
+			policyExist:  true,
+		},
+		{
+			description:  "rte api server network policy",
+			policyType:   "apiserver",
+			policyName:   "rte-egress-to-api-server",
+			component:    ComponentResourceTopologyExporter,
+			subComponent: "",
+			policyExist:  true,
+		},
+		{
+			description:  "rte metrics server network policy",
+			policyType:   "metrics",
+			policyName:   "ingress-to-rte-metrics",
+			component:    ComponentResourceTopologyExporter,
+			subComponent: "",
+			policyExist:  true,
+		},
+		{
+			description:  "scheduler controller default network policy",
+			policyType:   "default",
+			policyName:   "topology-aware-controller-default-deny-all",
+			component:    ComponentSchedulerPlugin,
+			subComponent: SubComponentSchedulerPluginController,
+			policyExist:  true,
+		},
+		{
+			description:  "topology-aware-controller api server network policy",
+			policyType:   "apiserver",
+			policyName:   "topology-aware-controller-egress-to-api-server",
+			component:    ComponentSchedulerPlugin,
+			subComponent: SubComponentSchedulerPluginController,
+			policyExist:  true,
+		},
+		{
+			description:  "scheduler default network policy",
+			policyType:   "default",
+			policyName:   "topology-aware-scheduler-default-deny-all",
+			component:    ComponentSchedulerPlugin,
+			subComponent: SubComponentSchedulerPluginScheduler,
+			policyExist:  true,
+		},
+		{
+			description:  "topology-aware-scheduler api server network policy",
+			policyType:   "apiserver",
+			policyName:   "topology-aware-scheduler-egress-to-api-server",
+			component:    ComponentSchedulerPlugin,
+			subComponent: SubComponentSchedulerPluginScheduler,
+			policyExist:  true,
+		},
+		{
+			description:  "wrong network policy",
+			policyType:   "Non existent policy",
+			policyName:   "Non existent policy",
+			component:    "",
+			subComponent: "",
+			policyExist:  false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			np, err := NetworkPolicy(tc.component, tc.subComponent, tc.policyType, "numaresources-operator")
+			if !tc.policyExist {
+				if err == nil {
+					t.Fatalf("expected error for non-existent policy %q, but got none", tc.policyType)
+				}
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("unexpected error for %q: %v", tc.policyType, err)
+			}
+
+			if np == nil {
+				t.Fatal("expected a network policy but got nil")
+			}
+
+			if np.Name != tc.policyName {
+				t.Fatalf("unexpected network policy name: got=%q, want=%q", np.Name, tc.policyName)
+			}
+		})
+	}
+}
